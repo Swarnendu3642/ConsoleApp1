b2f52688615a4121cdecca52e781a3e9d5a496d6
Swarnendu3642 <swarnenduroy3642@gmail.com>
2025-09-24 15:06:48 +0530

    practice

--- Program.cs
+++ Program.cs
@@ -8,6 +8,7 @@ namespace ConsoleApp1
             Console.WriteLine("Hello, World!");
             Console.WriteLine("From Github");
             Console.WriteLine("New Branch");
+            Console.WriteLine("master");
         }
     }
 }
